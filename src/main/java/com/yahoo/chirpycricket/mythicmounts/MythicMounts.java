ab24e950f00b9e37c29368df5ecce593aef93f7f
ChirpyC <chirpycricket@yahoo.com>
2022-03-17 22:53:14 -0400

    Added Patchouli load test.

--- src/main/java/com/yahoo/chirpycricket/mythicmounts/MythicMounts.java
+++ src/main/java/com/yahoo/chirpycricket/mythicmounts/MythicMounts.java
@@ -3,6 +3,8 @@ package com.yahoo.chirpycricket.mythicmounts;
 import net.fabricmc.api.ModInitializer;
 import software.bernie.example.GeckoLibMod;
 import software.bernie.geckolib3.GeckoLib;
+import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.common.book.BookRegistry;
 
 public class MythicMounts implements ModInitializer {
 
@@ -13,5 +15,10 @@ public class MythicMounts implements ModInitializer {
     {
         GeckoLib.initialize();
         GeckoLibMod.DISABLE_IN_DEV = true;
+
+        System.out.println("Any books loaded? " + !BookRegistry.INSTANCE.books.isEmpty());
+        for(Book b : BookRegistry.INSTANCE.books.values()){
+            System.out.println("Found book id: " + b.id);
+        }
     }
 }
